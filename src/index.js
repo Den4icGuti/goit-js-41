ddd40233d3eb19f204f6f27c14aa4a03ef2993fa
Den4icGuti <goos7412@gmail.com>
2021-11-15 20:20:15 +0200

    изучение методов массивов

--- src/index.js
+++ src/index.js
@@ -1490,3 +1490,942 @@
 // }
 
 
+// 
+
+
+// const persons = [
+
+//   { name: 'Vasya', age: 32, salary: 1500,online:false },
+
+//     {name:'Petya',age:36,salary:1700,online:true},
+
+//      {name:'Misha',age:42,salary:1900,online:true},
+
+// ];
+
+
+
+// console.table(persons);
+
+//Ищем пользователя по имени
+
+// const searchPersonToName = function (allName, fliendName) {
+  
+
+//   for (const personName of allName) {
+    
+//     console.log(personName.name)
+
+
+//     if (personName.name === fliendName) {
+      
+//       return 'Found';
+//     }
+
+//   }
+
+//   return 'Do not found!'
+
+// }
+
+
+// console.log(searchPersonToName(persons,'Vsya'))
+
+
+
+
+//Ищем пользователя online
+
+
+// const searchOnlinePerson = function (allPerson) {
+  
+//   const onlinePerson = [];
+//   for (const person of allPerson) {
+    
+   
+ 
+//     if (person.online) {
+      
+//       onlinePerson.push(person)
+//    }
+    
+
+//   }
+//   return onlinePerson;
+// }
+
+// console.log(searchOnlinePerson(persons))
+
+//Ищем пользователя offline
+
+
+
+// const searchPersonOffline = function (allPerson) {
+  
+
+//   const offlinePerson = [];
+
+
+//   for (const person of allPerson) { 
+
+    
+
+
+//     if (!person.online) {
+      
+//       offlinePerson.push(person)
+
+//     }
+ 
+//   }
+//   return offlinePerson
+// }
+
+// console.log(searchPersonOffline(persons))
+
+
+// const statusPerson = function (allPerson) {
+  
+//   const status = {
+
+//     online:[],
+
+//     offline: []
+//   }
+
+//   for (const person of allPerson) {
+    
+//     if (person.online) {
+      
+//       status.online.push(person);
+
+//     }
+
+//       status.offline.push(person);
+
+//   }
+// return status
+// }
+
+
+// console.log(statusPerson(persons))
+
+
+
+// const numbers = [0,40,25,...[1, 2, 3, 4],54,70,50];
+
+// console.log(numbers)
+
+
+
+// const person = {
+
+//   name: 'Jack',
+
+//   age: 28,
+  
+//   country: 'Las Wegas'
+
+
+// }
+
+
+// const keys = Object.keys(person);
+
+// const arrValues = [];
+
+// for (const key of keys) {
+  
+//   arrValues.push(key);
+
+
+//   console.log(arrValues)
+  
+ 
+// }
+
+
+
+
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+
+
+
+
+// function getAllValues(propName) {
+
+//   const result = [];
+
+
+//   for (const product of products) {
+    
+//     if (product.hasOwnProperty(propName)) {
+      
+//       result.push(product[propName]);
+
+//       console.log(result);
+//     }
+//   }
+  
+// return []
+
+// }
+
+
+// getAllValues('quantity')
+
+//Вщзвращаем значения по ключю массива
+
+// const persons = [
+
+//   { name: 'Den', surname: 'Hutin', age: 32 },
+//   { name: 'Vasya', surname: 'Pupkin', age: 36 },
+//   { name: 'Petya', surname: 'Sidorov', age: 40 }
+   
+// ]
+  
+
+
+// function getAllValues(propName) {
+  
+//   const result = [];
+
+//   for (const person of persons) {
+    
+//     if (person.hasOwnProperty(propName)) {
+      
+//       result.push(person[propName]);
+
+//       console.log(result);
+
+      
+//     }
+
+//   }
+//   return result
+
+// }
+
+
+
+// getAllValues('age')
+
+
+// const a = { name: 'Vasya', age: 32, country: { city: 'Zp' },z:10 };
+
+// const b = { name: 'Petya', age: 36, country: { city: 'Kyiv' },z:8 };
+
+
+// const c = {
+
+//   ...a,
+//  name:'Kolya',
+//   ...b,
+
+//  z:40
+// }
+
+// console.log(c)
+
+
+// const a = { a: 25, b: 75, g: { name: 'Den' } };
+
+// const b = { a: 75, b: 350, g: { name: 'Petya' } };
+
+
+// const c = {
+
+//   ...a,
+ 
+ 
+ 
+//   ...b,
+ 
+//    a: 45,
+
+// }
+
+// console.log(c)
+
+
+
+//Дустркуризация
+
+
+// const person = {
+
+//   name: 'Den',
+  
+//   surname: 'Hutin',
+  
+//   age:32,
+
+//   info: {
+
+//     birthday: 1988,
+    
+//     mounth: 'november',
+
+//     day: 28
+
+//   }
+
+// }
+
+
+
+// const { name, surname: patronumic, age,country = 'Zp',info } = person;
+
+// console.log(name)
+
+
+// const { birthday, mounth, day } = info;
+
+// console.log(birthday, mounth, day);
+
+
+
+// const arrname = ['den', 'vasya', 'petya'];
+
+
+// console.log(arrname.length - 1)
+
+
+// const arr = [25, 40, 70];
+
+// const newCopyArr = [...arr];
+
+// console.log(newCopyArr)
+
+
+
+
+
+// const cart = {
+
+//   items: [],
+  
+//   getitems() {
+    
+//     return this.items
+
+//   },
+//   add(product) {
+
+
+//     const newProduct = {
+
+//       ...product,
+//       quntity: 1
+      
+      
+
+//     }
+    
+//     this.items.push(newProduct)
+//   },
+
+//   remove(productName) { 
+
+//     const { items } = this
+
+//     for (let i = 0; i < items.length; i += 1) {
+
+//       const { name } = this.items[i];
+
+//       if (productName === name) {
+        
+//         console.log(`Нашли такой продукт ${productName}`)
+
+//         console.log('Индекс: ', i)
+
+
+//         items.splice(i, 1);
+        
+        
+
+//         return productName
+
+//       }
+
+     
+   
+//      }
+
+//   },
+  
+//   clear() { 
+
+//     this.items = [];
+
+//     return this.items
+
+//   },
+
+//   countTotalPrice() {
+
+//     const { items } = this;
+
+//     let total = 0;
+
+    
+
+   
+
+//     for (const  { price } of items) {
+      
+    
+     
+//       total += price;
+
+//     }
+
+//    return total
+//    },
+
+//   increaseQuantity() { },
+
+//   decreaseQuantity() { }
+
+// };
+
+
+// console.table(cart.getitems())
+
+// cart.add({ name:'apple', price:50 });
+
+// cart.add({ name: 'orange', price: 70 })
+// cart.add({ name:'lemon', price: 100 })
+// cart.add({ name: 'strawberry', price: 120 })
+// cart.add({ name: 'grape', price: 75 })
+
+
+
+
+
+// console.table(cart.getitems())
+
+
+// console.log('Total: ', cart.countTotalPrice())
+
+
+// cart.remove('apple');
+
+
+// console.table(cart.getitems())
+
+
+// cart.clear();
+
+
+// console.log(cart.clear())
+
+// console.log(cart.getitems())
+
+// cart.countTotalPrice();
+
+
+
+
+// const usr = {
+
+//   info: [
+//     { name: 'Den' },
+    
+//     { name: 'Vasya' },
+
+//     { name: 'Petya' },
+    
+//     {name:'Kolya'},
+   
+//   ],
+ 
+
+  
+
+
+//   getUserValue() {
+    
+//     const value = []
+
+//     for (const user of this.info) {
+      
+
+//       value.push(user);
+
+//     }
+//     return value
+
+//   },
+  
+//   addNewName(newName) {
+    
+   
+
+ 
+   
+//     for (const name of this.info) {
+     
+
+//       if (name.name === newName) {
+         
+//         return `this is ${newName} occupied by another user`;
+      
+//       }
+
+
+//           return 'Welcome: ',this.info.push(newName) 
+//    }
+
+    
+
+
+//   }
+
+// }
+
+
+
+
+// console.log(usr.addNewName())
+
+// console.log(usr.getUserValue())
+
+const names = ['Vasya', 'Petya', 'Kolya'];
+
+// const addNewName = function addName(newName) {
+  
+//   for (const name of names) {
+    
+//     if (name === newName) {
+      
+   
+//      return `This ${newName} is taken by another user.`
+
+//     }
+
+  
+
+//   }
+//   return `Welcome ${newName}`
+
+// }
+
+// console.log(addNewName('Denis'))
+
+
+// const removeName = function (userName) {
+  
+//   const indexName = names.indexOf(userName);
+
+//   for (const name of names) {
+    
+//     if (name === userName) {
+      
+//       return `There is no user with this ${userName}`
+
+
+//     }
+
+//   }
+
+//   return names.splice(1,indexName) + 'User delete'
+
+// }
+
+
+
+// console.log(removeName('Petya'))
+
+
+
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Dragon breath", price: 780 },
+//     { name: "Stone skin", price: 520 },
+//   ],
+ 
+//   getPotions() {
+//     return this.potions;
+//   },
+
+  // метод на добавление нового имени
+//   addPotion(newPotion) {
+    
+//     const {potions} = this;
+    
+//     for(const potion of potions){
+//     if (potion.name === newPotion.name) {
+//       return `Error! Potion ${newPotion.name} is already in your inventory!`;
+//     }
+// }
+//     return potions.push(newPotion);
+//   },
+
+  // метод удаления имени
+  // removePotion(potionName) {
+   
+  //   const {potions} = this;
+    
+  //   for(const potion of potions){
+    
+  //     if(potion.name === potionName){
+        
+  //     potions.splice(potions.indexOf(potion),1)
+      
+  //     return potion
+  //     }
+  //   }
+
+  
+
+    
+  // },
+  // метод обновления имени
+
+  // updatePotionName(oldName, newName) {
+   
+  //   const {potions} = this;
+    
+  //   for(const potion of potions){
+    
+  //    if (potion.name === oldName){
+     
+  //        potion.name = newName;
+       
+  //      return;
+  //    }
+    
+  //   }
+    
+
+  //   return `Potion ${oldName} is not in inventory! `;
+   
+  // },
+  // Change code above this line
+//};
+
+
+
+const SicoraShop = {
+
+  products: [
+
+  { name: 'Satcom T-505', price: 350 },
+
+  { name: 'Hopestar H-11', price: 470 },
+
+  { name: 'Hopestar H-45 Party', price: 650 },
+  
+  {name:'Hopestar H-01',price:1000}, 
+
+
+  ],
+
+  add(newProduct) {
+    
+    const { products } = this;
+
+    for (const product of products) {
+      
+      if (product.name === newProduct) {
+        
+         return `Such a ${newProduct} is already in the assortment`
+        
+      }
+     
+    
+    }
+   
+   return products.push(newProduct.name) + `Product ${newProduct} added`
+
+   
+  },
+
+
+  removeProduct(productName) {
+    
+    const { products } = this;
+
+    for (const product of products) {
+      
+      if (product.name === productName) {
+        
+        products.splice(products.indexOf(product), 1);
+
+       return products
+
+
+      }
+      
+    }
+
+   return `This  ${productName}  is not in the range`
+  },
+
+
+
+  updateProductName(oldName, newName) { 
+
+    const { products } = this;
+
+    for (const product of products) { 
+
+      if (product.name === oldName) { 
+
+        product.name = newName;
+
+        return `New name ${newName}  update`
+
+      }
+
+
+    }
+   
+    return `This ${oldName} is not on the list`;
+
+
+  },
+
+
+     countTotaPrice() {
+    
+
+    const { products } = this;
+
+
+    let total = 0;
+       
+       
+       for (const {  price  } of products) {
+      
+      total += price;
+
+    }
+
+   return total
+
+  }
+
+}
+
+
+
+  // console.log(SicoraShop.add('Hopestar H-24'))
+
+
+  // console.log(SicoraShop.removeProduct('Satcom T-505'))
+
+
+  // console.log(SicoraShop.updateProductName('Satcom T-505'))
+
+
+  // console.log('Total: ',SicoraShop.countTotaPrice())
+
+
+
+
+// Интеравалы
+
+// const call = function () {
+  
+
+//   console.log('Случай через 3 секунды');
+
+// }
+
+// console.log('перед таймаутом');
+
+
+// setTimeout(call, 3000);
+
+// console.log('Конец таймаута');
+
+//Работа с backend
+
+// const onReques = function (response) {
+  
+//   console.log('вызов функции onReques после успешного ответа бекенда')
+
+// console.log(response)
+// }
+
+
+// fetch('https://pokeapi.co/api/v2/pokemon')
+//   .then(res => res.json())
+
+//   .then(onReques)
+
+// Фильтр
+
+
+// const filter = function (array, test) {
+  
+//   const filterArr = [];
+
+//   for (const arr of array) {
+    
+//     console.log(arr)
+//     console.log(test(arr))
+
+//     const passed = test(arr);
+
+//     if (passed) {
+      
+//       filterArr.push(arr)
+
+//     }
+
+//   }
+//   return filterArr;
+// };
+
+
+// const call = function (value) {
+  
+//   return value >= 3;
+// }
+
+
+// const r1 = filter([1,23,7,46,7,4,2,3],call);
+
+// console.log(r1)
+
+// const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], function (value) {
+  
+
+//   return value <= 4;
+
+// })
+
+// console.log(r2)
+
+
+
+// const fruits = [
+
+//   { name: 'apple', quantity: 150, isFresh: true },
+//   { name: 'gpapes', quantity: 220, isFresh: false },
+//   { name: 'orange', quantity: 120, isFresh: true },
+   
+// ];
+
+
+// const fruitsQuntity = function (fruit) {
+  
+//   return fruit.quantity >= 100;
+
+
+// }
+
+// const res = filter(fruits, fruitsQuntity);
+
+// console.log(res)
+
+
+
+//Замыкание
+
+
+// const makeDish = function (sheffName, dish) {
+  
+//   console.log(`${sheffName}  готовит  ${dish}`)
+// }
+
+
+
+// makeDish('Денис', 'солянку')
+
+
+// const funName = function (name) {
+  
+//   const num = 12;
+//   const message = 'Hello';
+//   const allName = function (surname) {
+//     console.log(num)
+//     console.log(message)
+//    console.log(`Name: ${name} Surname:  ${surname}`)
+
+//   }
+
+//   return allName;
+// }
+
+
+// const den = funName('Den');
+
+// console.dir(den)
+
+// den('Hutin')
+
+
+
+// Фильтрация одинаковых элементов массива свойства обьекта
+
+const persons = [
+
+  {
+   
+    names: ['Vasya', 'Petya'],
+
+    age: [25, 40],
+    friends: ["Goldie Gentry", "Briana Decker"],
+
+  },
+
+
+  {
+
+    names: ['Vasya', 'Petya'],
+    
+    age: [26, 43],
+    friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes","Briana Decker","Goldie Gentry"],
+  }
+  
+
+
+];
+
+const allName = [...persons].sort((a, b) => a.friends.length - b.friends.length)
+
+.map((person) => person.names)
+
+
+console.log(allName)
+
+
+// const allAge = persons.flatMap(person => person.age);
+
+// const arrAge = allAge.filter((age, index, array)=> array.indexOf(age) === index )
+
+// console.log(arrAge)
+
+
+// chaning цепочка методов 
+
+// const sorted = [...users].filter(user => user.rating >= rat)
+//   .map(user => user.name)
+//   .sort((a,b)=> a.localeCompare(b))
+
+//   console.log(sorted)
+
+
+// const numbers = [21, 2, 4, 7, 89, 5, 6];
+
+
+// const sorted = numbers.filter(num => num > 2)
+//   .map(num => num * 2)
+//   .sort((a, b) => a - b);
+
+//   console.log(sorted)
+
+
+
